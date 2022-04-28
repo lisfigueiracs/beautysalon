c61d92bb2fb1c876a70c2caa4e0d9d4faf5d3a3d
lisfigueiracs <lislaynefigueira@gmail.com>
2022-04-28 19:47:41 -0300

    finalização scroll

--- main.js
+++ main.js
@@ -16,3 +16,17 @@ for(const link of links) {
         nav.classList.remove('show')
     })
 }
+
+// mudar o header da pagina quando der o scroll 
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+    if(this.window.scrollY >= navHeight) {
+        // scroll é maior que a altura do header 
+        header.classList.add('scroll')
+    } else {
+        // menor que a altura do header
+        header.classList.remove('scroll')
+    }
+})
